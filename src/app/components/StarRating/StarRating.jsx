ac2fa1c4e1580a819ffb7874f0d8a839d4bc6804
Max Lyovin <lyovin.maxim@gmail.com>
2021-06-21 16:41:00 +0200

    feat: StarRating component, added some assets

---
+++ src/app/components/StarRating/StarRating.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ReactComponent as Star } from '../../../assets/svg/star.svg';
+import { ReactComponent as StarOutline } from '../../../assets/svg/star-outline.svg';
+import './StarRating.scss';
+
+const StarsRating = ({ maxStars, productRating }) => {
+    const stars = [];
+
+    for (let i = 0; i < maxStars; i++) {
+        let star;
+        if (i < productRating) {
+            star = <Star key={i} className='rating__star rating__star--active' />
+        } else {
+            star = <StarOutline key={i} className='rating__star rating__star--outline' />
+        }
+
+        stars.push(star)
+    }
+
+    return (
+        <div className='rating'>
+            {stars}
+        </div>
+    );
+}
+
+StarsRating.propTypes = {
+    maxStars: PropTypes.number,
+    productRating: PropTypes.number
+}
+
+export default StarsRating;
