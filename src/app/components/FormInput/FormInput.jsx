aa20111937767c0b1635eb62858d2735b0960d07
Max Lyovin <lyovin.maxim@gmail.com>
2021-06-23 10:28:04 +0200

    feat: FormInput component

---
+++ src/app/components/FormInput/FormInput.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './FormInput.scss';
+
+const FormInput = ({ labelValue, name, onChangeFuncton, type = 'text', value }) => {
+    return (
+        <div className='d-flex flex-column mb-2'>
+            <label htmlFor={name}>
+                {labelValue}
+            </label>
+            <input type={type} className='p-3 my-2 login-form__input' name={name} placeholder={`Enter ${name}`} value={value} onChange={onChangeFuncton} />
+        </div>
+    );
+}
+
+export default FormInput;
